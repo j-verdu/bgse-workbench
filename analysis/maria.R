4f5e12649c6b6ecea71ff6814000d83ffd04659a
gseuser <gseuser@GSEALU.svtcloud.com>
2014-12-11 11:18:12 +0100

    create querie with tryout file to find the category with the most sales

--- analysis/maria.R
+++ analysis/maria.R
@@ -3,7 +3,10 @@ library(RMySQL)
 
 db = dbConnect(MySQL(), user='root', password='root', dbname='ecommerce', host='localhost')
 
-result = dbSendQuery(db, "SELECT * from customers")
+result = dbSendQuery(db, "select sum(D.UnitPrice*D.Quantity), P.CategoryID, C.CategoryName
+from order_details D, products P, categories C
+where D.ProductID=P.ProductID and P.CategoryID=C.CategoryID
+                     group by P.CategoryID;")
 
 data = fetch(result, n=-1)
 
