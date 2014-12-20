433d6dbf4b3a8754c81a1e0b6abd990ac19bc3aa
gseuser <gseuser@GSEALU.svtcloud.com>
2014-12-20 16:22:03 +0100

    removing

--- web/data_and_analysis.php
+++ web/data_and_analysis.php
@@ -27,17 +27,7 @@ limit 10";
 	query_and_print_table($query);
 	echo "Returns the 10 products that have made most sales in the ";
 
-	$query = "SELECT * FROM ecommerce.products LIMIT 10";
-	query_and_print_table($query);
-	echo "Comment 2";
-
-	// query_and_print_graph: Needs two columns: the first one with labels, the second one with values of the graph
-
-
-	$query = "SELECT ProductName, UnitPrice FROM ecommerce.products ORDER BY UnitPrice ASC LIMIT 10";
-	$title = "Top cheap products";
-	query_and_print_graph($query,$title,"Euros");
-	echo "Comment 4";
+	
 ?>
 		</div>
 		<div id="analysis" style="display: none">
